6af3bbe6bb0d3d09dbc17587572a763cdd02936e
MaximoB14 <maximoi.becerra@gmail.com>
2025-11-16 16:07:34 -0300

    actualizar

--- ms-operaciones/src/main/java/ar/edu/utnfc/backend/ms_operaciones/config/OpenApiConfig.java
+++ ms-operaciones/src/main/java/ar/edu/utnfc/backend/ms_operaciones/config/OpenApiConfig.java
@@ -1,17 +1,35 @@
 package ar.edu.utnfc.backend.ms_operaciones.config;
 
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class OpenApiConfig {
     @Bean
     OpenAPI api() {
-        return new OpenAPI().info(new Info()
-                .title("MS Operaciones")
-                .version("1.0")
-                .description("Rutas, tramos, asignación y tracking."));
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new io.swagger.v3.oas.models.security.SecurityScheme()
+                                .name("bearerAuth")
+                                .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("MS Operaciones")
+                        .version("1.0")
+                        .description("Rutas, tramos, asignación y tracking."));
     }
 }
